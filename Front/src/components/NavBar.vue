37e57df0f5734d940157235467bc76ce3742c9a6
BigFlat98 <hdj9836@naver.com>
2026-03-26 15:30:11 +0900

    logo file modify

--- Front/src/components/NavBar.vue
+++ Front/src/components/NavBar.vue
@@ -18,7 +18,7 @@ async function handleLogout() {
   <nav class="relative flex items-center justify-between px-4 md:px-8 py-3 border-b border-gray-100 bg-white z-50">
     <!-- Logo -->
     <router-link to="/" class="flex items-center transition-opacity hover:opacity-80">
-      <img src="/HadaLogo_nobg.png" alt="Hadaboni Logo" class="h-16 md:h-20 object-contain" />
+      <img src="/HadaLogo.png" alt="Hadaboni Logo" class="h-16 md:h-20 object-contain" />
     </router-link>
 
     <!-- Desktop Centered Links -->
